4bba697b1807d540fd562f473034569c66bd05a0
nosoyyo <oyyoson@gmail.com>
2018-06-15 20:55:50 +0800

    added: '我的统计'、'群统计' commands

--- youshan.py
+++ youshan.py
@@ -1,4 +1,5 @@
 import time
+import jieba.analyse
 from datetime import datetime
 from sortedcontainers import SortedDict
 
@@ -65,7 +66,7 @@ def genMsgId() -> str:
 
 
 def stats(puid: str=None) -> str:
-    corpus = getTodayCorpus()
+    corpus = getCorpus('today')
     today_n = len(corpus)
     if not puid:
         sds = {}
@@ -77,7 +78,9 @@ def stats(puid: str=None) -> str:
             sd_rank[sd] = len(sds[sd])
         sd_rank = {value: key for key, value in sd_rank.items()}
         sd_rank = SortedDict(sd_rank)
-        chart_title = '今天贵群刷了{}条:\n'
+
+        # build chart
+        chart_title = '今天贵群刷了{}条:\n'.format(today_n)
         charts = []
         sd_rank_list = list(sd_rank)
         sd_name_list = list(sd_rank.values())
@@ -108,21 +111,44 @@ def stats(puid: str=None) -> str:
         return stats
 
 
-def getTodayCorpus() -> list:
+def getCorpus(arg='today'):
     '''
-    return a list of dict like
+    :param `today`: `list` return a list of dict like
     {'serial': '164',
     'user': 'ch',
     'date': '20180615',
     'time': 1528996039.0,
     'type': 'Text',
     'text': '回不来了，已经晚了'}
+    :param `raw_today`: `str` return a str of everything people said
     '''
     with open('db', 'r') as f:
-        all = f.read().split('\n')
+        all_dict = f.read().split('\n')
+    all_dict.remove('')
+    # today for instance: `20180615`
     today = formatToday()
-    return [eval(item.split(' = ')[1])
-            for item in all if today in item.split(' = ')[0]]
+    corpus_today = [eval(item.split(' = ')[1])
+                    for item in all_dict if today in item.split(' = ')[0]]
+    if arg == 'today':
+        return corpus_today
+    elif arg == 'raw_today':
+        return ''.join(
+            [item['text'] for item in corpus_today if item['text'] is not None])
+    elif arg == 'raw_alltime':
+        corpus_all = [eval(item.split(' = ')[1]) for item in all_dict]
+        return ''.join(
+            [item['text'] for item in corpus_all if item['text'] is not None])
+
+
+def groupKeyword(arg: str='today') -> str:
+    corpus = ''
+    if arg == 'today':
+        corpus = getCorpus('raw_today')
+    elif arg == 'alltime':
+        corpus = getCorpus('raw_alltime')
+    kw = jieba.analyse.textrank(
+        corpus, topK=20, withWeight=False, allowPOS=('ns', 'n'))
+    return '\n'.join(kw)
 
 
 def persistize(msg):
@@ -149,7 +175,10 @@ def getQuery(msg: Message) -> list:
     ['01f0700d', 73b7c671']
     '''
     names = msg.text.replace('\u2005', '').split('@')[2:]
-    return [the_group.search(name)[0].puid for name in names]
+    if names:
+        return [the_group.search(name)[0].puid for name in names]
+    else:
+        return msg.text.split(' ')[1]
 
 
 @bot.register(Group, None, except_self=False)
@@ -162,6 +191,14 @@ def deal(msg):
         if msg.is_at:
             queries = getQuery(msg)
             if not queries:
+                the_group.send(stats())
+            elif '今日关键词' in queries:
+                the_group.send(groupKeyword('today'))
+            elif '全部关键词' in queries:
+                the_group.send(groupKeyword('alltime'))
+            elif '我的统计' in queries:
+                the_group.send(stats(msg.member.puid))
+            elif '群统计' in queries:
                 the_group.send(stats())
             else:
                 for query in queries:
